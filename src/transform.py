1c51d85311809ae1e448aeb819107cb9abbde24e
Yuma Kinoshita <ykinoshita@tmu.ac.jp>
2021-03-23 18:05:06 +0900

    feat: prepared new files

---
+++ src/transform.py
@@ -0,0 +1,38 @@
+import hdrpy.tmo as tmo
+
+from deepy.data.transform import Transform
+
+
+class ExposureCompensation(Transform):
+    def __init__(self):
+        pass
+
+    def __call__(self, image):
+        pass
+
+
+class RandomExposure(Transform):
+    def __init__(self):
+        pass
+
+    def __call__(self, image):
+        pass
+
+    def get_param(self):
+        pass
+
+
+class ReinhardTMO(Transform):
+    def __init__(self):
+        pass
+
+    def __call__(self, image):
+        pass
+
+
+class EilertsenTMO(Transform):
+    def __init__(self):
+        pass
+
+    def __call__(self, image):
+        pass
