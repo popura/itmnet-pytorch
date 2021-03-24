69d28801cfb6466fe07fe11bec15d34aabbda4f2
Yuma Kinoshita <ykinoshita@tmu.ac.jp>
2021-03-24 14:13:23 +0900

    feat: implemented train

--- src/util.py
+++ src/util.py
@@ -1,5 +1,4 @@
 import typing
-from typing import List
 
 import numpy as np
 
@@ -9,7 +8,14 @@ from torchsummary import summary
 
 from omegaconf import DictConfig
 
-from deepy.nn.model import VGG2d
+from itmnet import ITMNet
+
+
+HDR_IMG_EXTENSIONS = ('.hdr', '.exr', '.pfm')
+
+
+def is_valid_file(x):
+    return has_file_allowed_extension(x, HDR_IMG_EXTENSIONS)
 
 
 def is_same_config(cfg1: DictConfig, cfg2: DictConfig) -> bool:
@@ -39,11 +45,11 @@ def set_random_seed(seed: int) -> None:
     torch.backends.cudnn.benchmark = False
 
 
-def get_model(classes: List[str], cfg: DictConfig) -> nn.Module:
+def get_model(classes: list[str], cfg: DictConfig) -> nn.Module:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    if cfg.model.name == "vgg2d":
-        net = VGG2d(
+    if cfg.model.name == "itmnet":
+        net = ITMNet(
             in_channels=1,
             num_classes=len(classes),
             base_channels=cfg.model.param.base_channels,
